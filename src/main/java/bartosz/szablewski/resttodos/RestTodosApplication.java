c86b018c0f43ed6f527b2fda8dbf7adb3d9a828c
bszablewski <bartosz.szablewski123@wp.pl>
2020-09-13 14:02:49 +0200

    Initial commit

---
+++ src/main/java/bartosz/szablewski/resttodos/RestTodosApplication.java
@@ -0,0 +1,13 @@
+package bartosz.szablewski.resttodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bartosz/szablewski/resttodos/RestTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package bartosz.szablewski.resttodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
